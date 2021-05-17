8dd7199fba31a6c9b3d67df0197bdce4bde87e6b
Bohdan Klius <bohdanklius@gmail.com>
2021-05-17 23:33:17 +0300

    Updates

---
+++ precache-manifest.d99f9fec7a0cc1f0e4ec467e8834af78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ffeeea0eddba8c256652f575234c59",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b18070212c0815732bf6",
+    "url": "./static/css/main.ddcb9aad.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18070212c0815732bf6",
+    "url": "./static/js/main.557943fc.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
